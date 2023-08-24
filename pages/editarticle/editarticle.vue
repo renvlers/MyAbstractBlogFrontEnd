710fa730a3a475ba89591a9831babd0e143066ce
renvlers <3102069270@qq.com>
2023-08-25 01:45:14 +0800

    ok

---
+++ pages/editarticle/editarticle.vue
@@ -0,0 +1,238 @@
+<template>
+	<view class="global-view" :class="{ 'dark-theme': darkTheme }">
+		<view class="container">
+			<uni-forms label-position="top" label-width="100%">
+				<uni-forms-item :label="translations.标题" name="title" :required="true">
+					<uni-easyinput v-model="formData.title" type="text" :maxlength="20"
+						:placeholder="translations.请输入标题" />
+				</uni-forms-item>
+				<uni-forms-item :label="translations.分类" name="type" :required="true">
+					<uni-data-select v-model="formData.type" style="background-color: white; border-radius: 10rpx;"
+						:localdata="types" :placeholder="translations.请选择分类" :clear="true"></uni-data-select>
+				</uni-forms-item>
+				<uni-forms-item :label="translations.简介" name="tabloid" :required="true">
+					<uni-easyinput type="text" v-model="formData.tabloid" :placeholder="translations.请输入简介" />
+				</uni-forms-item>
+				<uni-forms-item :label="translations.标签" name="tags" :required="false">
+					<uni-easyinput type="text" v-model="formData.tags" :placeholder="translations.请输入标签" />
+				</uni-forms-item>
+				<uni-forms-item :label="translations.封面" name="cover" :required="false">
+					<image v-if=isCoverExists :src="formData.cover" alt="上传封面">
+						<button v-if="isCoverExists" class="red-button"
+							@click="cancelUploadCover()">{{translations.取消上传}}</button>
+						<button v-else class="green-button" @click="uploadCover()">{{translations.上传封面}}</button>
+				</uni-forms-item>
+			</uni-forms>
+		</view>
+		<view class="container" id="content">
+			<span>{{translations.文章内容}}</span>
+			<male-editor class="text-editor" v-model="formData.content"></male-editor>
+		</view>
+		<view class="container">
+			<button class="green-button" @click="postArticle()">{{translations.修改}}</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	import Chinese from '@/languages/zh-CN'
+	import English from '@/languages/en-US'
+	export default {
+		data() {
+			return {
+				isCoverExists: false,
+				gridStyle: {
+					"height": "400rpx", // 边框高度
+					"width": "100%", // 边框宽度
+					"border": { // 如果为 Boolean 值，可以控制边框显示与否
+						"color": "#ccc", // 边框颜色
+						"width": "1px", // 边框宽度
+						"style": "solid", // 边框样式
+						"radius": "10rpx" // 边框圆角，支持百分比
+					}
+				},
+				formData: {
+					aid: 0,
+					title: "",
+					type: 0,
+					tabloid: "",
+					tags: "",
+					content: "",
+					cover: ""
+				},
+				types: [],
+				translations: this.language === "en-US" ? English : Chinese
+
+			}
+		},
+		methods: {
+			postArticle() {
+				console.log(this.formData);
+				let postDto = {
+					a_id: this.formData.aid,
+					a_content: this.formData.content,
+					a_cover_url: this.formData.cover,
+					a_tabloid: this.formData.tabloid,
+					a_tags: this.formData.tags,
+					a_title: this.formData.title,
+					cg_id: this.formData.type
+				};
+				console.log(postDto);
+				if (postDto.a_title == "" || +postDto.cg_id == 0 || postDto.a_tabloid == "" || postDto.a_content.trim() ===
+					"") {
+					uni.showToast({
+						title: "请填写完整信息",
+						icon: "error"
+					});
+				} else {
+					console.log(postDto);
+					uni.request({
+						url: "/api/article/update",
+						method: "PUT",
+						data: postDto,
+						success: (response) => {
+							console.log(response);
+							uni.showToast({
+								title: "修改成功",
+								icon: "success",
+								success: () => {
+									let aid = this.formData.aid;
+									this.formData.content = "";
+									this.formData = {};
+									setTimeout(() => {
+										uni.redirectTo({
+											url: `/pages/article/article?aid=${aid}`
+										})
+									}, 1500);
+								}
+							});
+						},
+						fail: (error) => {
+							uni.showToast({
+								title: "修改失败",
+								icon: "error"
+							});
+						}
+					});
+				}
+			},
+			uploadCover() {
+				uni.chooseImage({
+					count: 1,
+					success: (imgResponse) => {
+						uni.uploadFile({
+							url: "/api/file/upload",
+							filePath: imgResponse.tempFilePaths[0],
+							success: (response) => {
+								console.log(response.data);
+								this.formData.cover = JSON.parse(response.data).data;
+								this.isCoverExists = true;
+							},
+							fail: () => {
+								uni.showToast({
+									title: "封面上传失败",
+									icon: "error"
+								})
+							}
+						});
+					}
+				});
+			},
+			cancelUploadCover() {
+				this.formData.cover = "",
+					this.isCoverExists = false;
+			}
+		},
+		onLoad(para) {
+			uni.request({
+				url: `/api/article/getarticle/${para.aid}`,
+				success: (response) => {
+					this.formData.aid = response.data.data.a_id;
+					this.formData.title = response.data.data.a_title;
+					this.formData.type = response.data.data.cg_id;
+					this.formData.tabloid = response.data.data.a_tabloid;
+					this.formData.tags = response.data.data.a_tags;
+					this.formData.content = response.data.data.a_content;
+					this.formData.cover = response.data.data.a_cover_url;
+					if (this.formData.cover === "" || this.formData.cover === null)
+						this.isCoverExists = false;
+					else
+						this.isCoverExists = true;
+				},
+				fail: (error) => {
+					console.error(error);
+					uni.showToast({
+						title: "文章信息加载失败",
+						icon: "error"
+					});
+				}
+			});
+			uni.setNavigationBarTitle({
+				title: this.translations.修改文章
+			});
+			if (this.darkTheme) {
+				uni.setNavigationBarColor({
+					frontColor: '#ffffff', // 这是文字颜色，设置为白色
+					backgroundColor: '#2a2a2a', // 这是背景颜色，设置为深色
+				});
+			} else {
+				uni.setNavigationBarColor({
+					frontColor: '#000000',
+					backgroundColor: '#f8f8f8'
+				});
+			}
+			if (!uni.getStorageSync("user")) {
+				uni.showToast({
+					title: "请先登录账号",
+					icon: "error",
+					success: () => {
+						setTimeout(() => {
+							uni.redirectTo({
+								url: "/pages/login/login"
+							})
+						}, 1500);
+					}
+				});
+			} else {
+				uni.request({
+					url: "/api/category/list",
+					success: (res) => {
+						for (let i of res.data.data) {
+							this.types.push({
+								text: i.cg_name,
+								value: i.cg_id,
+								disable: false
+							});
+						}
+					},
+					fail: () => {
+						uni.showToast({
+							title: "分类信息查询失败",
+							icon: "error"
+						});
+					}
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	#content {
+		padding: 10rpx;
+		width: calc(100% - 60rpx);
+	}
+
+	#content span {
+		margin: 20rpx 0 0 20rpx;
+	}
+
+	.text-editor {
+		width: 100%;
+		height: auto;
+		margin: 0;
+		padding: 0;
+		box-shadow: none;
+		background-color: none;
+	}
+</style>
---
+++ pages/spark/spark.vue
@@ -0,0 +1,162 @@
+<template>
+	<view class="global-view">
+		<view class="container">
+			<text style="font-size: 1.25em">发送您的问题：</text>
+			<input style="background-color: white; font-size: 1em; margin: 20rpx 0 0; border-radius: 10rpx; padding: 10rpx" v-model="TEXT"/>
+		</view>
+		<button class="green-button" style="margin: 30rpx; width: calc(100% - 60rpx);" @click="sendToSpark()">发送</button>
+		<view class="container">
+			<text style="font-size: 1.25em">回复：</text>
+			<text style="width: 100%;flex-wrap:wrap;font-size: 1em; margin:20rpx 0 0;">{{sparkResult}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	import * as base64 from "base-64"
+	import CryptoJS from '../../static/crypto-js/crypto-js.js'
+	import parser from '../../static/fast-xml-parser/src/parser'
+	import * as utf8 from "utf8"
+	export default {
+		data() {
+			return {
+				TEXT: '',
+				APPID: '625a2d35', // 控制台获取填写
+				APISecret: 'NDY1NDJiZGZmYzNjYWJiYmRlMTkxYmJm',
+				APIKey: '181ae46996eff260d5d4334f3311c4bb',
+				sparkResult: '',
+			}
+		},
+		methods: {
+			async sendToSpark() {
+				let myUrl = await this.getWebSocketUrl();
+				this.sparkResult="";
+				let realThis = this;
+				this.socketTask = uni.connectSocket({
+					//url: encodeURI(encodeURI(myUrl).replace(/\+/g, '%2B')),
+					url: myUrl,
+					method: 'GET',
+					success: res => {
+						console.log(res, "ws成功连接...", myUrl)
+						realThis.wsLiveFlag = true;
+					}
+				})
+				realThis.socketTask.onError((res) => {
+					console.log("连接发生错误", res)
+				})
+				realThis.socketTask.onOpen((res) => {
+					console.info("wss的onOpen成功执行...", res)
+					// 第一帧..........................................
+					console.log('open成功...')
+					let params = {
+						"header": {
+							"app_id": this.APPID,
+							"uid": "aef9f963-7"
+						},
+						"parameter": {
+							"chat": {
+								"domain": "general",
+								"temperature": 0.5,
+								"max_tokens": 1024
+							}
+						},
+						"payload": {
+							"message": {
+								"text": [{
+										"role": "user",
+										"content": "我叫大王，请记住我的名字"
+									},
+									{
+										"role": "assistant",
+										"content": "好的，大王。我会记住您的名字。"
+									},
+									{
+										"role": "user",
+										"content": this.TEXT
+									}
+								]
+							}
+						}
+					};
+					console.log("发送第一帧...", params)
+					realThis.socketTask.send({ // 发送消息，，都用uni的官方版本
+						data: JSON.stringify(params),
+						success() {
+							console.log('第一帧发送成功')
+						}
+					});
+				});
+
+				// 接受到消息时
+				realThis.socketTask.onMessage((res) => {
+					console.log('收到API返回的内容：', res.data);
+					let obj=JSON.parse(res.data)
+					// console.log("我打印的"+obj.payload);
+					let dataArray= obj.payload.choices.text;
+					for(let i=0;i<dataArray.length;i++){
+						realThis.sparkResult =realThis.sparkResult+ dataArray[i].content
+					}
+					// realThis.sparkResult =realThis.sparkResult+ 
+					let temp = JSON.parse(res.data)
+					// console.log("0726",temp.header.code)
+					if (temp.header.code !== 0) {
+						console.log(`${temp.header.code}:${temp.message}`);
+						realThis.socketTask.close({
+							success(res) {
+								console.log('关闭成功', res)
+								realThis.wsLiveFlag = false;
+							},
+							fail(err) {
+								  console.log('关闭失败', err)
+							}
+						})
+					}
+					if (temp.header.code === 0) {
+						if (res.data && temp.header.status === 2) {
+							/* let dataArray= obj.payload.choices.text;
+							for(let i=0;i<dataArray.length;i++){
+								realThis.sparkResult =realThis.sparkResult+ dataArray[i].content
+							} */
+							setTimeout(() => {
+								realThis.socketTask.close({
+									success(res) {
+										console.log('关闭成功', res)
+									},
+									fail(err) {
+										// console.log('关闭失败', err)
+									}
+								})
+							}, 1000)
+						}
+					}
+				})
+			},
+			// 鉴权
+			getWebSocketUrl() {
+				return new Promise((resolve, reject) => {
+					// 请求地址根据语种不同变化 https://spark-api.xf-yun.com/v1.1/chat
+					var url = "wss://spark-api.xf-yun.com/v1.1/chat";
+					var host = "spark-api.xf-yun.com";
+					var apiKeyName = "api_key";
+					var date = new Date().toGMTString();
+					var algorithm = "hmac-sha256";
+					var headers = "host date request-line";
+					var signatureOrigin = `host: ${host}\ndate: ${date}\nGET /v1.1/chat HTTP/1.1`;
+					var signatureSha = CryptoJS.HmacSHA256(signatureOrigin, this.APISecret);
+					var signature = CryptoJS.enc.Base64.stringify(signatureSha);
+					var authorizationOrigin =
+						`${apiKeyName}="${this.APIKey}", algorithm="${algorithm}", headers="${headers}", signature="${signature}"`;
+					var authorization = base64.encode(authorizationOrigin);
+					url = `${url}?authorization=${authorization}&date=${encodeURI(date)}&host=${host}`;
+
+					// console.log(url)
+					resolve(url); // 主要是返回地址
+				});
+			},
+		}
+	}
+</script>
+
+<style>
+
+</style>
